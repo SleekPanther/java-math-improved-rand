7340c73e0a554a33b1c207a125be3cf166b093e0
SleekPanther <macrobyte2@gmail.com>
2017-01-08 10:17:27 -0500

    better variable name

--- MathImprovedRandom.java
+++ MathImprovedRandom.java
@@ -1,12 +1,12 @@
 public class MathImprovedRandom {
 
 	public static void main(String[] args) {
-
+		System.out.println(MathImprovedRandom.random());
 	}
 	
 	public static double random(){
 		return Math.random();
 	}
 	
-	
+	//public static double random(){
 }
