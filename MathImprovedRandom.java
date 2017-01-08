fc3a36c0b89dc798f4a9caf302c58847d8223c58
SleekPanther <macrobyte2@gmail.com>
2017-01-08 15:47:53 -0500

    fully functional

--- MathImprovedRandom.java
+++ MathImprovedRandom.java
@@ -1,12 +1,46 @@
 public class MathImprovedRandom {
-
-	public static void main(String[] args) {
-		System.out.println(MathImprovedRandom.random());
-	}
 	
 	public static double random(){
 		return Math.random();
 	}
 	
-	//public static double random(){
+	public static double random(double upperBound){
+		return Math.random() * upperBound;
+	}
+	
+	public static double random(double lowerBound, double upperBound){
+		double range = upperBound - lowerBound;
+		return (Math.random() * range) + lowerBound;
+	}
+	
+	
+	public static int randInt(){
+		return (int) (Math.random() *Integer.MAX_VALUE);
+	}
+	
+	public static int randInt(int upperBound){
+		return (int) (Math.random() * upperBound);
+	}
+	
+	public static int randInt(int lowerBound, int upperBound){
+		int range = upperBound - lowerBound +1;
+		return (int) (Math.random() * range + lowerBound);
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(MathImprovedRandom.random(10,20));
+		for(int i=0; i< 30; i++){
+			if( (int)MathImprovedRandom.random(10,20) == 20){
+				System.out.println("found double  term");
+			}
+		}
+		
+		System.out.println(MathImprovedRandom.randInt(10,20));
+		for(int i=0; i< 30; i++){
+			if( MathImprovedRandom.randInt(10,20) == 20){
+				System.out.println("found integer term");
+			}
+		}
+	}
 }
