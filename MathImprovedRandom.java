4262028a5acff3ccda66c3713fc4f8bc0693c128
Noah Patullo <SleekPanther@users.noreply.github.com>
2017-03-26 06:55:38 -0400

    Update MathImprovedRandom.java

--- MathImprovedRandom.java
+++ MathImprovedRandom.java
@@ -1,27 +1,33 @@
 public class MathImprovedRandom {
 	
+	//Returns a double between 0 and 1
 	public static double random(){
 		return Math.random();
 	}
 	
+	//Returns a double between 0 and upperBound
 	public static double random(double upperBound){
 		return Math.random() * upperBound;
 	}
 	
+	//Returns a `double` between lowerBound and upperBound
 	public static double random(double lowerBound, double upperBound){
 		double range = upperBound - lowerBound;
 		return (Math.random()*range) + lowerBound;
 	}
 	
 	
+	//Returns an integer between 0 and Integer.MAX_VALUE (like Random class nextInt() )
 	public static int randInt(){
 		return (int) (Math.random() * Integer.MAX_VALUE);		//use MAX_VALUE to simulate behavior of nextInt() from the defualt Random class 
 	}
 	
+	//Returns an integer between 0 and upperBound.  EXCLUDING upperBound
 	public static int randInt(int upperBound){
 		return (int) (Math.random() * upperBound);
 	}
 	
+	//Returns an integer between lowerBound and upperBound. INCLUDING upperBound
 	public static int randInt(int lowerBound, int upperBound){
 		int range = upperBound - lowerBound +1;
 		return (int) (Math.random()*range + lowerBound);
